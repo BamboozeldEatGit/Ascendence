9bffb12015e0b6e9dfe8e057f5a23873fe8abe7c
BamboozeldEatGit <113732239+BamboozeldEatGit@users.noreply.github.com>
2024-10-08 17:53:35 -0500

    ahhh

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <nav>
+            <div className='w-full h-16 bg-slate-700 flex items-center p-2 lg:text-center bg-opacity-45 bg fixed'>
+                <h1 className='text-white text-4xl pl-1'>Atedence</h1>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default page
